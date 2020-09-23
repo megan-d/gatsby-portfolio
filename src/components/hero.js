c4d71b03901d848ecb7734bb70f096532dbcad30
Megan D <45672063+megan-d@users.noreply.github.com>
2020-09-23 13:37:51 -0600

    Begin to build portfolio

---
+++ src/components/hero.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Hero = (props) => (
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+      }}
+    >
+    <p>Hi, I'm Megan</p>
+    <p>I build full-stack and front-end web applications</p>
+    <p>You can check out my recent projects below and my github profile <a href='https://www.github.com/megan-d'>here</a></p>
+    </div>
+)
+
+export default Hero;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,9 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
+import Hero from "./hero";
+import "./layout.css";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,25 +17,24 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
+          width: '100vw',
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
+         <Hero />
         <main>{children}</main>
         <footer style={{
-          marginTop: `2rem`
+          marginTop: `2rem`,
+          textAlign: 'center'
         }}>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
         </footer>
       </div>
-    </>
   )
 }
 
