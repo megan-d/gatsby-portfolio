ca6e73e3090f4892f64f90c6970580d1131b85ac
Megan D <45672063+megan-d@users.noreply.github.com>
2022-04-23 21:17:13 -0600

    Update to remove front-end

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,6 +1,6 @@
-import React from "react";
-import logo from "../images/logo2.svg";
-import github from '../images/GitHub-Mark-Light-64px.png';
+import React from "react"
+import logo from "../images/logo2.svg"
+import github from "../images/GitHub-Mark-Light-64px.png"
 
 const Hero = props => (
   <div
@@ -8,24 +8,29 @@ const Hero = props => (
       margin: `0`,
       padding: "0",
       width: "100vw",
-      height: '90vh',
+      height: "90vh",
       backgroundColor: "orange",
-      overflow: 'scroll'
+      overflow: "scroll",
     }}
   >
-    <img src={logo} alt="logo" className='logo' />
+    <img src={logo} alt="logo" className="logo" />
     <div className="hero-text">
       <h1>Hi, I'm Megan.</h1>
-      <h3>I build full-stack and front-end web applications.</h3>
-      <div className='hero-subtitle'>
-      <p>
-        Check out my recent projects below and my GitHub profile{" "}
-        <a href="https://www.github.com/megan-d" className='anchor' target="_blank"
-          rel="noopener noreferrer">here.</a> 
-      </p>
-      <img src={github} alt='github-logo' className='github-logo'/>
+      <h3>I build full-stack web applications.</h3>
+      <div className="hero-subtitle">
+        <p>
+          Check out my recent projects below and my GitHub profile{" "}
+          <a
+            href="https://www.github.com/megan-d"
+            className="anchor"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            here.
+          </a>
+        </p>
+        <img src={github} alt="github-logo" className="github-logo" />
       </div>
-      
     </div>
   </div>
 )
